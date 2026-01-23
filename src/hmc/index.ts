b72855657fc6864bce42bf7d0352a4b8d4d4e509
StefanSko <Stefan.sko@gmail.com>
2026-01-23 17:51:02 +0100

    feat: export all modules from main index

---
+++ src/hmc/index.ts
@@ -0,0 +1,4 @@
+export type { HMCState, HMCInfo, HMCConfig } from './types';
+export type { HMCSampler } from './builder';
+export { HMC, HMCBuilder } from './builder';
+export { createHMCKernel } from './kernel';
---
+++ src/metrics/index.ts
@@ -0,0 +1,2 @@
+export type { Metric } from './types';
+export { createGaussianEuclidean } from './gaussian-euclidean';
